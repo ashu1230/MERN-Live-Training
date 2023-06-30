12f41c1c267c285b9b78a14b1b5345f2096d8157
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-06-30 22:44:07 +0530

    Update Qun-2.js

--- Assignment/Assignment 1/Qun-2.js
+++ Assignment/Assignment 1/Qun-2.js
@@ -11,3 +11,28 @@ function capital(input) {
 
 capital('learn javascript from brain mentors');
 'Learn Javascript From Brain Mentors'
+
+1.Write a JavaScript program to display the current day and time in the following format.
+Today is : wednesday.
+Current time is : 10 PM : 30 : 38
+
+function date(){
+    var List = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var dateobj = new Date();
+    var getcurrentDay = dateobj.getDay();
+    var getcurrentHour = dateobj.getHours();
+    var getcurrentsecond = dateobj.getSeconds();
+    var getcurrentMinutes = dateobj.getMinutes();
+
+    var DayOrNight = getcurrentHour >= 12 ? "PM" : "AM";
+
+    var Output = `Today Is : ${List[getcurrentDay]}
+    Current Time Is :${getcurrentHour} ${DayOrNight} : ${getcurrentMinutes} : ${getcurrentsecond}`;
+
+    console.log(Output);
+}
+
+date();
+
+Today Is : Friday
+Current Time Is :22 PM : 41 : 25

ecc31995766dae9e8edb769d0f499a7798627b86
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-06-30 23:35:38 +0530

    Final

--- Assignment/Assignment-1/2.js
+++ Assignment/Assignment-1/2.js
@@ -1,6 +1,6 @@
-2.Write a JavaScript program to get the current date.
-Expected Output :
-mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy..
+// 2.Write a JavaScript program to get the current date.
+// Expected Output :
+// mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy..
 
 
 

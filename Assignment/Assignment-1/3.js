923bb801b9b54cf35a6d05592903a5b63a726808
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-06-30 23:01:05 +0530

    Final

---
+++ Assignment/Assignment-1/3.js
@@ -0,0 +1,13 @@
+3.WAP to print area and perimeter of rectangle , taking input from the user , using prompt
+
+
+function AreaCal() {
+    var length = parseInt (prompt("Value Of Length"));
+    var breadth = parseInt (prompt("Value of Breadth"));
+
+    console.log("Area Is:- " + length * breadth);
+}
+
+
+
+AreaCal();

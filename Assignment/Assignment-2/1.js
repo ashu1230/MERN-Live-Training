9f5f6f6e439c8cba8cfb69e455437a30a6524303
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-06-30 23:37:13 +0530

    Update 1.js

--- Assignment/Assignment-2/1.js
+++ Assignment/Assignment-2/1.js
@@ -1,1 +1,19 @@
-value
+Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word into upper case.
+Example string : ‘learn javascript from brain mentors’
+Expected Output : ‘Learn Javascript From Brain Mentors
+
+
+
+function capital(input) {
+    var w = input.split(' ');
+    var capW = [];
+    w.forEach(element => {
+       capW.push(element[0].toUpperCase() + element.slice(1, element.length)); 
+    });
+    return capW.join(' ');
+}
+
+
+
+capital('learn javascript from brain mentors');
+'Learn Javascript From Brain Mentors'

f640b6b5e9f2375c826c9d4945d995be71903f9f
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-07-01 20:53:05 +0530

    Create 1.js

---
+++ Assignment/Assignment-3/1.js
@@ -0,0 +1,10 @@
+// 1.Write a function calculate that takes three arugments from the user, say num1 , num2, and operation(1==add, 2==subtract, 
+// 3==multiply, 4==divide ).Don`t use switch statement or if else statements
+ 
+// 1.add,
+// 2.subtract,
+// 3.multiply,
+// 4.divide 
+
+// write the calc function for all the operations
+

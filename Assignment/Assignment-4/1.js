bca2729c135f531c913f9f5b5f048e39268d76fa
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-07-01 14:08:49 +0530

    Final

--- Assignment/Assignment-4/1.js
+++ Assignment/Assignment-4/1.js
@@ -1,6 +1,6 @@
 // 1. Find Occurance of a target value in given array;
 // arr= [19, 22, 18, 19, 16, 18, 19, 21, 24];
-// target = 19;
+// target = 19;.
 
 arr= [19, 22, 18, 19, 16, 18, 19, 21, 24];
 

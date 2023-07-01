348b7fc7a4713782d6d6f9d885eb472b3233bb23
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-07-01 13:43:05 +0530

    Final

---
+++ Assignment/Assignment-4/6.js
@@ -0,0 +1,16 @@
+ //  6. Write a function to remove a specific element from an array.
+
+	// arr = [1,2,3,4,5];
+	// remove = 3
+
+	// output :- [1,2,4,5]
+
+
+arr = [1,2,3,4,5];
+(5) [1, 2, 3, 4, 5]
+
+console.log("Remove:- "+arr.splice(2,1), "Output:- ",[arr]);
+
+Remove:- 3 
+Output:-  [1, 2, 4, 5]
+

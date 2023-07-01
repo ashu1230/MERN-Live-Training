0057328a06a742afd68fd1b62773ce29025b09ab
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-07-01 12:47:25 +0530

    Create 3.js

---
+++ Assignment/Assignment-4/3.js
@@ -0,0 +1,6 @@
+// 3. Write a function that takes a string as input and returns the number of vowels (a, e, i, o, u) in the string.
+// Count of vowels in string;
+// ex:- str = "brainmentors"
+// 		output: count = 4
+
+

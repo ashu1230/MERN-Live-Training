47b5102b0772baa484839915e5d0dd08cf29b46f
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-07-01 12:46:03 +0530

    Create 2.js

---
+++ Assignment/Assignment-4/2.js
@@ -0,0 +1,24 @@
+// 2 . Reverse a given string using reverse() method 
+// input = "BrainMentors"
+// output = "srotnemniarB"
+
+arr = 'BrainMentors';
+
+'BrainMentors'
+
+arr = 'BrainMentors';
+
+out = arr.split("");
+(12) ['B', 'r', 'a', 'i', 'n', 'M', 'e', 'n', 't', 'o', 'r', 's']
+
+out = out.reverse();
+(12) ['s', 'r', 'o', 't', 'n', 'e', 'M', 'n', 'i', 'a', 'r', 'B']
+
+Output = out.join("");
+'srotneMniarB'
+
+console.log(Output);
+VM5256:1 srotneMniarB
+
+console.log("Output is:"+Output);
+VM5286:1 Output is:srotneMniarB

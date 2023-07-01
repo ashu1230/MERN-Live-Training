5d4bb66ec3e6a8fc60df9881c210e158fc959afc
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-07-01 13:01:48 +0530

    Create 5.js

---
+++ Assignment/Assignment-4/5.js
@@ -0,0 +1,7 @@
+// 5.Write a function to reverse the order of elements in an array.
+// don't Use reverse() Method 
+// ex:- var arr = ['a','b','c','d','e','f']
+			
+// 		output = ['f','e','d','c','b','a']
+
+

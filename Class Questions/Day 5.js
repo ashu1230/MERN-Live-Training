b7ed0b27c7d67b3cb6a2116728b7e4b8184798c3
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-07-01 12:18:42 +0530

    Create Day 5.js

---
+++ Class Questions/Day 5.js
@@ -0,0 +1,3 @@
+// 1. create an array with numerics and go to arr and check the maximum value in array.
+// 2. List Of name and there are two type mane first name and last name, and pic the name and create new array 
+// 3. array of songs Duration, type(new, old), name, count total new and old songs.             

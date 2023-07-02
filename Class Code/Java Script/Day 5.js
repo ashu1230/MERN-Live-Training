cb5d14465c3dc0d8de654383eb152a7abd4434a8
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-07-03 00:06:05 +0530

    Final

--- Class Code/Java Script/Day 5.js
+++ Class Code/Java Script/Day 5.js
@@ -1,20 +1,4 @@
-// 4. Create a function Employee, having id, name, basic_Salary as arguments.
 
-// Compute HRA, DA, TA, GS, NS.
-
-// HRA is 30% of Basic Salary.
-// DA is 10% of Basic Salary.
-// TA is 20% of Basic Salary.
-
-// GS = Basic Salary + HRA + DA + TA;
-
-// TAX DEDUCTION is 10% of GS.
-
-// NS = GS - TAX DEDUCTION.
-
-// NOte : Use Basic Salary in Lexical Scope 
-// Print salary slip of the employee.
-undefined
 function golbal (a){
         function perent (b){
             function local (c){}

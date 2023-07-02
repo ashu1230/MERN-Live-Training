75cf988dac670b59473427fd4c9997997f460ba1
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-07-02 23:16:02 +0530

    Create 4.js

---
+++ Assignment/Assignment-2/4.js
@@ -0,0 +1,32 @@
+// Write a JS Function to validate Email Address
+// Must contains @
+// Must ends with .com/.org/.edu
+
+
+function validateEmailAddress(email) {
+  if (email.indexOf('@') === -1) {
+    return false;
+  }
+  const validDomains = ['.com', '.org', '.edu'];
+  let validDomain = false;
+  for (let i = 0; i < validDomains.length; i++) {
+    if (email.endsWith(validDomains[i])) {
+      validDomain = true;
+      break;
+    }
+  }
+  return validDomain;
+}
+
+undefined
+const email = 'test@example.com';
+console.log(validateEmailAddress(email));
+
+VM1103:2 true
+undefined
+const email = 'example.org';
+console.log(validateEmailAddress(email)); 
+VM1115:2 false
+undefined
+const email = 'test@example.net';
+console.log(validateEmailAddress(email));

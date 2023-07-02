f2fb39b846fd5406a5b964fb86a769e3238e2c39
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-07-02 22:49:31 +0530

    Final

--- Assignment/Assignment-2/1.js
+++ Assignment/Assignment-2/1.js
@@ -4,3 +4,23 @@
 // O/p Even Place (9+4)
 // Odd Place 2+1
 
+function EvenOdd(num) {
+  var numString = num.toString();
+  var evenSum = 0;
+  var oddSum = 0;
+  for (var i = 0; i < numString.length; i++) {
+    var digit = parseInt(numString[i]);
+    if ((i + 1) % 2 === 0) {
+      evenSum += digit; 
+    } else {
+      oddSum += digit; 
+    }
+  }
+  console.log("Even Sum:", evenSum);
+  console.log("Odd Sum:", oddSum);
+}
+
+
+EvenOdd(2914);
+Even Sum: 13
+Odd Sum: 3

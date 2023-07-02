6cd9f2aef248d6d74d142cd946eb3c614e9cc649
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-07-02 23:30:40 +0530

    Final

--- Assignment/Assignment-3/1.js
+++ Assignment/Assignment-3/1.js
@@ -8,3 +8,27 @@
 
 // write the calc function for all the operations
 
+
+
+function calculate(num1, num2, ope) {
+  var operations = [
+    (a, b) => a + b,
+    (a, b) => a - b,
+    (a, b) => a * b,
+    (a, b) => a / b,
+  ];
+  var selectedOpe = operations[ope - 1];
+  if (selectedOpe) {
+    return selectedOpe(num1, num2);
+  } else {
+        return'Invalid operation!';
+  }
+}
+console.log(calculate(8, 5, 1));
+console.log(calculate(8, 2, 2));
+console.log(calculate(12, 1, 4));
+console.log(calculate(15, 8, 5));
+VM1445:15 13
+VM1445:16 6
+VM1445:17 12
+VM1445:18 Invalid operation!
